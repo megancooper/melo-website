2a50f1c89f51e295b68960a1652a62fe29afe92d
megancooper <megancooper1000@gmail.com>
2021-08-19 23:00:13 -0500

    Fix feedback secret env variable

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,9 @@
-const {NEXT_PUBLIC_MELO_API} = process.env;
+const MELO_API = process.env.NEXT_PUBLIC_MELO_API;
+const FEEDBACK_SECRET = process.env.NEXT_FEEDBACK_SECRET || '';
 
 const Api = {
   subscribe: async (email: string) => {
-    const res = await fetch(`${NEXT_PUBLIC_MELO_API}/api/stay-updated`, {
+    const res = await fetch(`${MELO_API}/api/stay-updated`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
@@ -20,12 +21,12 @@ const Api = {
     return null;
   },
   feedback: async (email: string, feedback: string) => {
-    const res = await fetch(`${NEXT_PUBLIC_MELO_API}/feedback`, {
+    const res = await fetch(`${MELO_API}/feedback`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        FEEDBACK_SECRET: process.env.NEXT_FEEDBACK_SECRET || '',
+        FEEDBACK_SECRET: FEEDBACK_SECRET || '',
       },
       body: JSON.stringify({
         email: email || '',
