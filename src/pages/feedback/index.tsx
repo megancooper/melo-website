7fa363e5b2fc7295a5d8a45cae70727b44540b0f
Megan Cooper <megancooper1000@gmail.com>
2022-03-06 21:22:09 -0600

    Remove unused variable :(

--- src/pages/feedback/index.tsx
+++ src/pages/feedback/index.tsx
@@ -4,20 +4,17 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import Form from './form';
 
-const Feedback = () => {
-  const x = 3;
-  return (
-    <>
-      <Head>
-        <title>Feedback</title>
-      </Head>
-      <div className="lg:h-screen flex flex-col">
-        <Header />
-        <Form />
-      </div>
-      <Footer />
-    </>
-  );
-};
+const Feedback = () => (
+  <>
+    <Head>
+      <title>Feedback</title>
+    </Head>
+    <div className="lg:h-screen flex flex-col">
+      <Header />
+      <Form />
+    </div>
+    <Footer />
+  </>
+);
 
 export default Feedback;
